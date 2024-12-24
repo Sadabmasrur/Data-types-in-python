572e14aa6a6eecdad9089d2b7cb3c39bfa267856
Sadab Masrur <sadabmasrur08@gmail.com>
2024-12-24 11:48:59 +0600

    acp

---
+++ Congratulations.py
@@ -0,0 +1,11 @@
+msg="congratulations brother!! hope you will do better in life"
+
+print(msg.capitalize())
+
+print(msg.upper())
+print(msg.lower())
+
+print(msg.count("o"))
+
+print(len(msg))
+print(msg.startswith("c"))
