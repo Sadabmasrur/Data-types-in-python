afc009c6a8b40224273335dc62074438c45169c2
Sadab Masrur <sadabmasrur08@gmail.com>
2024-12-24 11:36:39 +0600

    first commit

---
+++ String operations.py
@@ -0,0 +1,10 @@
+str="Rayan"
+print(str[2])
+
+str2="Hello Mr. "
+print(str2+str)
+
+print(str[0:4:1])
+
+revText=str[::-1]
+print(revText)
---
+++ datatypes.py
@@ -0,0 +1,11 @@
+name=input("Enter the Name: ")
+print(type(name))
+
+age=int(input("Enter the Age: "))
+print(type(age))
+
+height=float(input("Enter the Height: "))
+print(type(height))
+
+isStudent=bool(input("Are you a Student[True/False]: "))
+print(type(isStudent))
